fc3d966cf652ddf8b0af80bb30c1cdbe92a448f3
pyrocat <i@pyroc.at>
2014-03-01 18:14:03 -0800

    working now

--- src/algorithm_w/core.clj
+++ src/algorithm_w/core.clj
@@ -21,25 +21,27 @@
 
 (defmethod ftv :default [t]
   (match (vec t)
-    [t1 '-> t2] (union (ftv t1) (ftv t2))
+    [t1 '-> t2]      (union (ftv t1) (ftv t2))
     [:forall vars τ] (difference (ftv τ) vars)))
+
+(defn fresh-var [] (gensym))
 
 ;;; substitution
 
-(defmulti apply-subst type)
+(defmulti apply-subst (fn [x y] (type y)))
 
 (defmethod apply-subst ::type-env [S env]
   (mapval #(apply-subst S %) env))
 
 (defmethod apply-subst ::type-const [S t] t)
 
-(defmethod apply-subst ::type-var   [S t]
-  (if-let [t [S t]] t t))
+(defmethod apply-subst ::type-var [S t]
+  (if-let [t (S t)] t t))
 
 (defmethod apply-subst :default [S t]
   (match (vec t)
     [t1 '-> t2]      [(apply-subst S t1) '-> (apply-subst S t2)]
-    [:forall vars t] [:forall vars (apply-subst (dissoc S vars) t)]))
+    [:forall vars τ] [:forall vars (apply-subst (dissoc S vars) τ)]))
 
 (defn compose
   "Compose type substitutions, right-to-left, i.e. should have
@@ -60,7 +62,7 @@
   ([σ]
    (match (vec σ)
      [:forall vars τ]
-     (let [S (into {} (for [v vars] [v (gensym)]))]
+     (let [S (into {} (for [v vars] [v (fresh-var)]))]
        (apply-subst S τ)))))
 
 ;; unification
@@ -73,13 +75,14 @@
 
 (derive java.lang.Object ::_)
 
-(defmulti unify #([(type %1) (type %2)]))
+(defmulti unify (fn [x y] [(type x) (type y)]))
+
+(defmethod unify [::type-var ::type-var] [t1 t2] {})
 
 (defmethod unify [::type-var ::_] [t1 t2]
-  (cond
-   (isa? (type t2) ::type-var) {}
-   (contains? (ftv t2) t1)     (throw (circular-unify t1 t2))
-   :else                       {t1 t2}))
+  (if (contains? (ftv t2) t1)
+    (throw (circular-unify t1 t2))
+    {t1 t2}))
 
 (defmethod unify [::_ ::type-var] [t1 t2]
   (unify t2 t1))
@@ -97,7 +100,9 @@
                                 (compose S2 S1))
     :else (throw (cannot-unify t1 t2))))
 
-(prefer-method unify [::type-var ::_] [::_ ::type-var])
+(prefer-method unify [::type-var ::type-var] [::type-var ::_])
+(prefer-method unify [::type-var ::type-var] [::_ ::type-var])
+(prefer-method unify [::type-var ::_]        [::_ ::type-var])
 
 ;;; W
 
@@ -106,7 +111,7 @@
 (derive java.lang.Long      ::int)
 (derive java.lang.Boolean   ::bool)
 
-(defmulti W type)
+(defmulti W (fn [x y] (type x)))
 
 (defmethod W ::var [v env]
   (if-let [σ (get env v)]
@@ -119,14 +124,39 @@
 (defmethod W :default [e env]
   (match (vec e)
     [e1 e2] (let [[t1 S1] (W e1 env)
-                  [t2 S2] (W (apply-subst S1 e2) env)
-                  β       (gensym)
+                  [t2 S2] (W e2 (apply-subst S1 env))
+                  β       (fresh-var)
                   S3      (unify (apply-subst S2 t1) [t2 '-> β])]
               [(apply-subst S3 β) (compose S3 S2 S1)])
-    ['fn [x] e1] (let [β       (gensym)
+    ['fn [x] e1] (let [β       (fresh-var)
                        [t1 S1] (W e1 (assoc env x [:forall #{} β]))]
                    [[(apply-subst S1 β) '-> t1] S1])
     ['let [x e1] e2] (let [[t1 S1] (W e1 env)
                            closure (generalize t1 env)
                            [t2 S2] (W e2 (assoc env x closure))]
                        [t2 (compose S2 S1)])))
+
+;;; desugar
+
+;;; type inference
+
+(def ^:dynamic *default-type-env*
+  {'+ '[:forall #{} [:int -> [:int -> :int]]]})
+
+(defn infer
+  ([e]
+   (infer e *default-type-env*))
+  ([e env]
+   (let [[t S] (W e env)]
+     (apply-subst S t))))
+
+;;; tests
+
+(infer '+)
+(infer '(+ 1))
+(infer '(fn [x] x))
+(infer '(fn [x] ((+ x) x)))
+(infer '(let [f (fn [x] x)] f))
+
+;; one-liner to clear current namespace
+;; (map #(ns-unmap *ns* %) (keys (ns-interns *ns*)))
